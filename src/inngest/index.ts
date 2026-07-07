21f73850c8c2a3049c62b10b177a880125ad6af3
Liong Cheng Lex <chenglex1@gmail.com>
2026-07-07 20:33:57 +0800

    feat(v4-p3): Inngest batch orchestrator + generate-all dispatch endpoint

--- src/inngest/index.ts
+++ src/inngest/index.ts
@@ -4,12 +4,15 @@
  *
  * Post-pivot (PRD v3.0): most generation is user-driven from the
  * Timeline Editor. Inngest remains for long-running jobs that still
- * make sense as background work (music, final render, publish).
+ * make sense as background work (music, final render, publish) and,
+ * as of v4 P3, the batch "Generate all" orchestrator.
  */
 export { inngest } from "./client";
 
 import { generateMusicFn } from "./functions/generate-music";
+import { generateBatchFn } from "./functions/generate-batch";
 
 export const functions = [
   generateMusicFn,
+  generateBatchFn,
 ];
